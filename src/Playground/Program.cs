b66753bea2b23a3c3d01fdf559e27d8a1b9dfbcb
Nikita Melnik <nekit.melnik@gmail.com>
2020-03-04 14:47:06 +0200

    Add editor config and global.json

---
+++ src/Playground/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(someFunc);
+            t.Name = "Thread1";
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("Main thread Running");
+            Console.ReadKey();
+        }
+
+        static void someFunc()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.Name} started");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.Name} completed");
+        }
+    }
+}
