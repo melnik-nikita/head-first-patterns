f0f714471d92c010cf8ae73ab2eb92c2977bda45
Nikita Melnik <nekit.melnik@gmail.com>
2020-07-02 12:47:40 +0300

    Add Singleton pattern example

---
+++ src/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics.Contracts;
+
+namespace Singleton
+{
+    public sealed class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+        private static readonly object sync = new object();
+
+        private static ChocolateBoiler instance = null;
+
+        public string Test {get;} => "string";
+        public static ChocolateBoiler Instance {
+            get
+            {
+                // Если это условие не выполнится, то никакие блокировки не нужны, поскольку
+                // экземпляр синглтона уже создан
+                if (instance is null)
+                {
+                    // Но поскольку никаких блокировок сделано не было, есть вероятность,
+                    // что произошло переключение контекста и другой поток успел создать
+                    // экземпляр синглтона, поэтому захватываем блокировку и проверяем
+                    // наличие экземпляра еще раз
+                    lock(instance)
+                    {
+                        if (instance is null)
+                        {
+                            // Да, экземпляр таки не создался, давайте-ка создадим его
+                            instance = new ChocolateBoiler();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public ChocolateBoiler GetInstance()
+        {
+            // Если это условие не выполнится, то никакие блокировки не нужны, поскольку
+            // экземпляр синглтона уже создан
+            if (instance is null)
+            {
+                // Но поскольку никаких блокировок сделано не было, есть вероятность,
+                // что произошло переключение контекста и другой поток успел создать
+                // экземпляр синглтона, поэтому захватываем блокировку и проверяем
+                // наличие экземпляра еще раз
+                lock(instance)
+                {
+                    if (instance is null)
+                    {
+                        // Да, экземпляр таки не создался, давайте-ка создадим его
+                        instance = new ChocolateBoiler();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                boiled = true;
+            }
+        }
+
+        public bool IsEmpty() => empty;
+        public bool IsBoiled() => boiled;
+    }
+}
---
+++ src/Singleton/Singleton.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel;
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>();
+
+        public static Singleton Instance {get;} => instance.Value;
+
+        private Singleton()
+        {}
+    }
+}
